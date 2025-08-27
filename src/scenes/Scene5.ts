0a7d7a6cd2fea6f4c8ad0a77096f430c7f41fce0
Ederson Torresini <etorresini@ifsc.edu.br>
2025-08-27 00:00:57 -0300

    Revisar código inicial das equipes e cenas Fixes #48

--- src/scenes/Scene5.ts
+++ src/scenes/Scene5.ts
@@ -1,21 +1,11 @@
 import { Scene } from "phaser";
 
-export class Boot extends Scene {
+export class Scene5 extends Scene {
   constructor() {
-    super({ key: "Boot" });
+    super({ key: "Scene5" });
   }
 
-  preload() {
-    this.load.image("boi", "assets/scene5/boi.jpg");
-  }
+  preload() {}
 
-  create() {
-    this.add
-      .image(400, 225, "boi")
-      .setInteractive()
-      .on("pointerdown", () => {
-        this.scene.stop();
-        this.scene.start("Preloader");
-      });
-  }
+  create() {}
 }
