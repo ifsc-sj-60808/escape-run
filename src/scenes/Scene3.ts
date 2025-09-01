60adfa32d30ec6785c2d6d2688900ea926c8e6fd
Jeremas <gabriel.jc2006@aluno.ifsc.edu.br>
2025-09-01 22:10:48 +0000

    Updt Code cena3

--- src/scenes/Scene3.ts
+++ src/scenes/Scene3.ts
@@ -7,12 +7,12 @@ export class Scene3 extends Scene {
 
   preload() {
     //this.load.image('player', 'static/assets/scene3/sprite-piloto-personagem-cena3.png'); 
-    //this.load.image('room', 'static/assets/scene3/MapTest01.png'); //gabriel: fix sprite name
+    this.load.image('room', 'static/assets/scene3/map-test01.png'); //gabriel: fix sprite name
 
   }
 
   create() {
-    //this.add.image(0,0, 'room')
+    this.add.image(0,0, 'room')
     //this.physics.add.image(0,0, 'player');
     //player.body.setCollideWorldBounds(true); //player collides with the room's bounds
   }
