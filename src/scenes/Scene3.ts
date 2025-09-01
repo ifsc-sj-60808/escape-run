53ad700da5c153204193eba8e27c059b0697664c
anthony-ameho <anthony.mh@aluno.ifsc.edu.br>
2025-09-01 18:21:41 +0000

    criando o começo da movimentação

--- src/scenes/Scene3.ts
+++ src/scenes/Scene3.ts
@@ -5,7 +5,15 @@ export class Scene3 extends Scene {
     super({ key: "Scene3" });
   }
 
-  preload() {}
+  preload() {
+    //this.load.image('player', 'static/assets/scene3/sprite-piloto-personagem-cena3.png'); 
+    //this.load.image('room', 'static/assets/scene3/MapTest01.png'); //gabriel: fix sprite name
 
-  create() {}
+  }
+
+  create() {
+    //this.add.image(0,0, 'room')
+    //this.physics.add.image(0,0, 'player');
+    //player.body.setCollideWorldBounds(true); //player collides with the room's bounds
+  }
 }
