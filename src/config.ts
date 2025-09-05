a5faa305fae12af0d964aee3de74b76500161a1c
Ederson Torresini <etorresini@ifsc.edu.br>
2025-09-05 02:07:42 -0300

    Testar interações com objetos em tela e sons Fixes #70

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 import { Types } from "phaser";
-import { Boot } from "./scenes/Boot";
 import { Scene1 } from "./scenes/Scene1";
 import { Scene2 } from "./scenes/Scene2";
 import { Scene3 } from "./scenes/Scene3";
@@ -7,13 +6,25 @@ import { Scene4 } from "./scenes/Scene4";
 import { Scene5 } from "./scenes/Scene5";
 import { Scene7 } from "./scenes/Scene7";
 import { Scene8 } from "./scenes/Scene8";
+import { TestNumpad } from "./scenes/TestNumpad";
+import { TestPiano } from "./scenes/TestPiano";
 
 const config: Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 450,
   height: 800,
   parent: "game-container",
-  scene: [Scene4, Scene1, Scene2, Scene3, Boot, Scene5, Scene7, Scene8],
+  scene: [
+    Scene1,
+    Scene2,
+    Scene3,
+    Scene4,
+    Scene5,
+    Scene7,
+    Scene8,
+    TestNumpad,
+    TestPiano,
+  ],
   physics: {
     default: "arcade",
     arcade: {
