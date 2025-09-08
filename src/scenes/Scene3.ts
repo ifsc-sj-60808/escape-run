e55822a0613164eaf39726a1e770b071d1698ff3
anthony-ameho <anthony.mh@aluno.ifsc.edu.br>
2025-09-08 18:21:55 +0000

    Criar TileMap JSON para a cena 3 Fixes #65

--- src/scenes/Scene3.ts
+++ src/scenes/Scene3.ts
@@ -1,48 +1,49 @@
 import { Scene } from "phaser";
 
 export class Scene3 extends Scene {
-  
   private map!: Phaser.Tilemaps.Tilemap;
   private allTiles!: Phaser.Tilemaps.TilemapLayer;
   private player!: Phaser.GameObjects.Image;
+  tileset!: Phaser.Tilemaps.Tileset;
 
   private playerTileX = 0;
   private playerTileY = 0;
   private tileSize = 32;
-  
+
   constructor() {
     super({ key: "Scene3" });
   }
 
   preload() {
-    //this.load.image('player', 'static/assets/scene3/sprite-piloto-personagem-cena3.png'); 
+    //this.load.image('player', 'static/assets/scene3/sprite-piloto-personagem-cena3.png');
     //this.load.image('room', 'static/assets/scene3/map-test01.png');
     //this.load.image('player', 'static/assets/scene3/sprite-piloto-personagem-cena3.png');
 
-    this.load.tilemapTiledJSON("scene3", 'static/assets/scene3/tilemap'); //JSON, arrangement of tiles
-    this.load.image('tiles', 'static/assets/scene3/TileMapLayerPiloto.png'); // loading the floor and wall tiles, first 32x32px are the floor tiles, second 32x32px are the wall tiles
-    this.load.image('player', 'static/assets/scene3/c'); // loading the player sprite
+    this.load.tilemapTiledJSON("scene3", "static/assets/scene3/tilemap.json"); //JSON, arrangement of tiles
+    this.load.image("tiles", "static/assets/scene3/TileMapLayerPiloto.png"); // loading the floor and wall tiles, first 32x32px are the floor tiles, second 32x32px are the wall tiles
+    this.load.image("player", "static/assets/scene3/c"); // loading the player sprite
   }
 
- create() {
+  create() {
     // --- MAP & LAYERS ---
-    const map = this.make.tilemap({ key: "scene3" });
-    const tileset = map.addTilesetImage("mapTemplate", "tiles"); // name must match JSON
-    const allTiles = map.createLayer("Ground", tileset!, 0, 0);
+    this.map = this.make.tilemap({ key: "scene3" });
+    this.tileset = this.map.addTilesetImage("mapTemplate", "tiles");
+    if (this.tileset)
+      this.allTiles = this.map.createLayer("Ground", this.tileset, 0, 0);
 
     // Walls are tile index 2
-    allTiles.setCollision(2);
+    this.allTiles.setCollision(2);
 
     // codenames
-    this.map = map;
-    this.allTiles = allTiles;
-    this.tileSize = map.tileWidth;
+    //this.map = map;
+    //this.allTiles = allTiles;
+    //this.tileSize = map.tileWidth;
 
-// iniciar mais ou menos centralizado
+    // iniciar mais ou menos centralizado
     const startTileX = 5;
     const startTileY = 4;
-    const startX = map.tileToWorldX(startTileX) - this.tileSize / 2;
-    const startY = map.tileToWorldY(startTileY) - this.tileSize / 2;
+    const startX = this.map.tileToWorldX(startTileX) - this.tileSize / 2;
+    const startY = this.map.tileToWorldY(startTileY) - this.tileSize / 2;
 
     this.player = this.add.image(startX, startY, "player").setOrigin(0.5);
     this.playerTileX = startTileX;
