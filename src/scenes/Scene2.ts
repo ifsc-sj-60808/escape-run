e63c0c4aaeb3f9823567518bfe63043da756290a
Mimi <Yasmin.kv2006@aluno.ifsc.edu.br>
2025-09-18 12:59:41 +0000

    Finalizar código da cena 2 Fixes #102

--- src/scenes/Scene2.ts
+++ src/scenes/Scene2.ts
@@ -15,7 +15,18 @@ export class Scene2 extends Scene {
 
   // Piano
   keys!: Key[];
-  c!: Phaser.Sound.BaseSound;
+  do!: Phaser.Sound.BaseSound;
+  dos!: Phaser.Sound.BaseSound;
+  re!: Phaser.Sound.BaseSound;
+  res!: Phaser.Sound.BaseSound;
+  mi!: Phaser.Sound.BaseSound;
+  fa!: Phaser.Sound.BaseSound;
+  fas!: Phaser.Sound.BaseSound;
+  sol!: Phaser.Sound.BaseSound;
+  sols!: Phaser.Sound.BaseSound;
+  la!: Phaser.Sound.BaseSound;
+  las!: Phaser.Sound.BaseSound;
+  si!: Phaser.Sound.BaseSound;
   password!: string;
   enter!: Phaser.GameObjects.Sprite;
   display!: Phaser.GameObjects.Text;
@@ -27,51 +38,77 @@ export class Scene2 extends Scene {
 
   preload() {
     // Background
-    this.load.image("TestPiano-background", "assets/TestPiano/background.png");
+    this.load.image("background", "assets/scene2/background.png");
 
     // Piano
-    this.load.image("keyBlack", "assets/TestPiano/keyBlack.png");
-    this.load.image("keyWhite", "assets/TestPiano/keyWhite.png");
-    this.load.audio("C", "assets/TestPiano/c.mp3");
+    this.load.image("keyBlack", "assets/scene2/keyBlack.png"); // imagem das teclas pretas
+    this.load.image("keyWhite", "assets/scene2/keyWhite.png"); // imagem das teclas brancas
+    this.load.image("enter", "assets/scene2/enter.png"); // imagem do botão enter
+    this.load.image("clear", "assets/scene2/clear.png"); // imagem do botão clear
+    this.load.audio("do", "assets/scene2/do.mp3"); // som da tecla dó
+    this.load.audio("dos", "assets/scene2/dos.mp3"); // som da tecla dó sustenido
+    this.load.audio("re", "assets/scene2/re.mp3"); // som da tecla ré
+    this.load.audio("res", "assets/scene2/res.mp3"); // som da tecla ré sustenido
+    this.load.audio("mi", "assets/scene2/mi.mp3"); // som da tecla mi
+    this.load.audio("fa", "assets/scene2/fa.mp3"); // som da tecla fá
+    this.load.audio("fas", "assets/scene2/fas.mp3"); // som da tecla fá sustenido
+    this.load.audio("sol", "assets/scene2/sol.mp3"); // som da tecla sol
+    this.load.audio("sols", "assets/scene2/sols.mp3"); // som da tecla sol sustenido
+    this.load.audio("la", "assets/scene2/la.mp3"); // som da tecla lá
+    this.load.audio("las", "assets/scene2/las.mp3"); // som da tecla lá sustenido
+    this.load.audio("si", "assets/scene2/si.mp3"); // som da tecla si
   }
 
   create() {
     // Background
-    this.add.image(225, 400, "TestPiano-background");
+    this.add.image(225, 400, "background");
 
     // Timer
     this.timer = this.add.text(50, 100, "");
 
     // Piano
     this.display = this.add.text(50, 150, "");
-    this.c = this.sound.add("C");
+    this.do = this.sound.add("do");
+    this.dos = this.sound.add("dos");
+    this.re = this.sound.add("re");
+    this.res = this.sound.add("res");
+    this.mi = this.sound.add("mi");
+    this.fa = this.sound.add("fa");
+    this.fas = this.sound.add("fas");
+    this.sol = this.sound.add("sol");
+    this.sols = this.sound.add("sols");
+    this.la = this.sound.add("la");
+    this.las = this.sound.add("las");
+    this.si = this.sound.add("si");
     this.password = "";
 
     this.keys = [
-      { x: 32, y: 250, image: "keyWhite", text: "C", sound: this.c },
-      { x: 96, y: 250, image: "keyWhite", text: "D" },
-      { x: 160, y: 250, image: "keyWhite", text: "E" },
-      { x: 224, y: 250, image: "keyWhite", text: "F" },
-      { x: 288, y: 250, image: "keyWhite", text: "G" },
-      { x: 352, y: 250, image: "keyWhite", text: "A" },
-      { x: 416, y: 250, image: "keyWhite", text: "B" },
+      { x: 32, y: 300, image: "keyWhite", text: "do", sound : this.do },
+      { x: 96, y: 300, image: "keyWhite", text: "re", sound : this.re },
+      { x: 160, y: 300, image: "keyWhite", text: "mi", sound : this.mi },
+      { x: 224, y: 300, image: "keyWhite", text: "fa", sound : this.fa },
+      { x: 288, y: 300, image: "keyWhite", text: "sol", sound : this.sol },
+      { x: 352, y: 300, image: "keyWhite", text: "la",  sound : this.la },
+      { x: 416, y: 300, image: "keyWhite", text: "si", sound : this.si },
 
-      { x: 64, y: 218, image: "keyBlack", text: "C#" },
-      { x: 128, y: 218, image: "keyBlack", text: "D#" },
-      { x: 256, y: 218, image: "keyBlack", text: "F#" },
-      { x: 320, y: 218, image: "keyBlack", text: "G#" },
-      { x: 384, y: 218, image: "keyBlack", text: "A#" },
+      { x: 64, y: 268, image: "keyBlack", text: "do#", sound : this.dos },
+      { x: 128, y: 268, image: "keyBlack", text: "re#", sound : this.res },
+      { x: 256, y: 268, image: "keyBlack", text: "fa#", sound : this.fas },
+      { x: 320, y: 268, image: "keyBlack", text: "sol#", sound : this.sols },
+      { x: 384, y: 268, image: "keyBlack", text: "la#", sound : this.las },
     ];
 
+    // Botão clear
     this.clear = this.physics.add
-      .sprite(75, 700, "void")
+      .sprite(75, 700, "clear")
       .setInteractive()
       .on("pointerdown", () => {
         this.password = "";
       });
 
+    // Botão enter
     this.enter = this.physics.add
-      .sprite(375, 700, "void")
+      .sprite(375, 700, "enter")
       .setInteractive()
       .on("pointerdown", () => {
         (this.game as typeof MultiPlayerGame).mqttClient.publish(
@@ -86,12 +123,13 @@ export class Scene2 extends Scene {
         .setInteractive()
         .on("pointerdown", () => {
           if (key.sound) key.sound.play();
-          if (this.password.length < 5) this.password += key.text;
+          if (this.password.length < 10) this.password += key.text;
         });
 
-      this.add.text(key.x + 12, key.y, key.text, {
+      this.add.text(key.x - 10, key.y + 40, key.text, {
         fontSize: "12px",
-        color: "#000",
+        color: "#881753",
+        fontStyle: "bold",
       });
     });
   }
