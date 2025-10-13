58922ed9237f8a4152d97994e3866e16395f8346
Ederson Torresini <etorresini@ifsc.edu.br>
2025-10-13 14:40:39 -0300

    Atualizar lista de cenas

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,17 @@ import { Scene1 } from "./scenes/scene1";
 import { Scene2 } from "./scenes/scene2";
 import { Scene3 } from "./scenes/scene3";
 import { Scene4 } from "./scenes/scene4";
-import { Scene5 } from "./scenes/scene56";
+import { Scene5 } from "./scenes/scene5";
 import { Scene6 } from "./scenes/scene6";
 import { Scene7 } from "./scenes/scene7";
 import { Scene8 } from "./scenes/scene8";
+import { Scene9 } from "./scenes/scene9";
+import { Scene10 } from "./scenes/scene10";
+import { Scene11 } from "./scenes/scene11";
+import { Scene12 } from "./scenes/scene12";
+import { Scene13 } from "./scenes/scene13";
+import { Scene14 } from "./scenes/scene14";
+import { Scene15 } from "./scenes/scene15";
 
 const config: Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -27,6 +34,13 @@ const config: Types.Core.GameConfig = {
     Scene6,
     Scene7,
     Scene8,
+    Scene9,
+    Scene10,
+    Scene11,
+    Scene12,
+    Scene13,
+    Scene14,
+    Scene15,
   ],
   physics: {
     default: "arcade",
