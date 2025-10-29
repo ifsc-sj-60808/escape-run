1ecabe8dc18f77ec0a41e3080a558ef50a1e1e72
Ederson Torresini <etorresini@ifsc.edu.br>
2025-10-29 14:12:17 -0300

    Atualizar broker e aplicar formtação com black

--- devices/esp/template/main.py
+++ devices/esp/template/main.py
@@ -5,14 +5,13 @@ from time import sleep
 
 led = Pin(2, Pin.OUT)
 
-wifi_ssid = 'escape-run'
-wifi_password = 'escape-run'
+wifi_ssid = "escape-run"
+wifi_password = "escape-run"
 
-mqtt_client_id = 'room-14-0'
-#mqtt_broker = 'escape-run.sj.ifsc.edu.br'
-mqtt_broker = 'test.mosquitto.org'
-mqtt_topic_subscribe = 'escape-run/room/14/0'
-mqtt_topic_publish = 'escape-run/player/msg'
+mqtt_client_id = "room-14-0"
+mqtt_broker = "escape-run.sj.ifsc.edu.br"
+mqtt_topic_subscribe = "escape-run/room/14/0"
+mqtt_topic_publish = "escape-run/player/msg"
 
 
 def setup():
@@ -30,53 +29,53 @@ def blink():
 def panic():
     # Liberar todas as portas e travas
     pass
-    
-    
+
+
 def connect_wifi():
     wlan = network.WLAN()
     wlan.active(True)
     wlan.connect(wifi_ssid, wifi_password)
 
     while not wlan.isconnected():
-        print('Connecting to Wi-Fi...')
+        print("Connecting to Wi-Fi...")
         sleep(1)
-        
-    print('Connected to Wi-Fi!')
+
+    print("Connected to Wi-Fi!")
 
 
 def connect_mqtt():
     client = MQTTClient(mqtt_client_id, mqtt_broker)
     client.connect()
     client.set_callback(callback)
-    print('Connected to MQTT broker!')
+    print("Connected to MQTT broker!")
     led.on()
     return client
 
-    
+
 def callback(topic, payload):
     msg = payload.decode()
-    print('Received message:', msg)
-    
-    if msg == 'blink':
+    print("Received message:", msg)
+
+    if msg == "blink":
         blink()
-    elif msg == 'panic':
+    elif msg == "panic":
         panic()
-    elif msg == 'reset':
+    elif msg == "reset":
         reset()
 
 
 def subscribe(client):
     client.subscribe(mqtt_topic_subscribe)
-    print('Subscribed to device topic:', mqtt_topic_subscribe)
-    
-    
-if __name__ == '__main__':
+    print("Subscribed to device topic:", mqtt_topic_subscribe)
+
+
+if __name__ == "__main__":
     setup()
     connect_wifi()
     mqtt_client = connect_mqtt()
     subscribe(mqtt_client)
-        
+
     while True:
-        mqtt_client.publish(mqtt_topic_publish, 'ping')
+        #mqtt_client.publish(mqtt_topic_publish, "ping")
         mqtt_client.check_msg()
         sleep(1)
--- devices/rasp/scene4/fifo2mqtt/fifo2mqtt.py
+++ devices/rasp/scene4/fifo2mqtt/fifo2mqtt.py
@@ -15,14 +15,14 @@ RUNNING = True
 
 
 def fifo_create():
-    '''Create the FIFO if it doesn't exist.'''
-    
+    """Create the FIFO if it doesn't exist."""
+
     if not os.path.exists(FIFO_PATH):
         os.mkfifo(FIFO_PATH, 0o600)
 
 
 def fifo_open():
-    '''Open the FIFO for reading.'''
+    """Open the FIFO for reading."""
 
     fd = os.open(FIFO_PATH, os.O_RDONLY | os.O_NONBLOCK)
     fifo = os.fdopen(fd, "rb", buffering=0)
@@ -31,12 +31,12 @@ def fifo_open():
     if poller:
         poller.register(fd, select.POLLIN)
         return fifo, poller
-    
+
     return fifo, None
 
 
 def fifo_read(fifo, poller):
-    '''Read from the FIFO.'''
+    """Read from the FIFO."""
 
     if poller:
         events = poller.poll(int(POLL_TIMEOUT * 1000))
@@ -57,21 +57,21 @@ def fifo_read(fifo, poller):
 
 
 def fifo_close(fifo):
-    '''Close the FIFO.'''
+    """Close the FIFO."""
 
     if os.path.exists(FIFO_PATH):
         fifo.close()
 
 
 def fifo_delete():
-    '''Delete the FIFO.'''
-    
+    """Delete the FIFO."""
+
     if os.path.exists(FIFO_PATH):
         os.remove(FIFO_PATH)
 
 
 def mqtt_connect():
-    '''Connect to the MQTT broker.'''
+    """Connect to the MQTT broker."""
 
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)  # type: ignore
     client.connect(MQTT_BROKER, MQTT_PORT)
