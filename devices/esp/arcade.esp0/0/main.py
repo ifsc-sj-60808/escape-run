de5157b68dccc66fa3ad5434a4d5da9ff9fb686b
Jeremas <gabriel.jc2006@aluno.ifsc.edu.br>
2025-10-30 17:19:38 +0000

    atualização código mqtt cena3 esp0

--- devices/esp/arcade.esp0/0/main.py
+++ devices/esp/arcade.esp0/0/main.py
@@ -1,4 +1,4 @@
-##Codigo MQTT Esp0 
+##Codigo MQTT Esp0
 from machine import Pin, reset
 import network
 from umqtt.robust import MQTTClient
@@ -10,20 +10,21 @@ game_started = Pin(12, Pin.OUT)
 gerador = Pin(14, Pin.OUT)
 door = Pin(11, Pin.OUT)
 dispencer = Pin(10, Pin.OUT)
-danger = Pin(7, Pin OUT)
 
-wifi_ssid = 'escape-run'
-wifi_password = 'escape-run'
 
-mqtt_client_id = 'room-12-0'
-#mqtt_broker = 'escape-run.sj.ifsc.edu.br'
-mqtt_broker = 'test.mosquitto.org'
-mqtt_topic_subscribe = 'escape-run/room/12/0'
-mqtt_topic_publish = 'escape-run/player/msg'
+wifi_ssid = "escape-run"
+wifi_password = "escape-run"
+
+mqtt_client_id = "room-12-0"
+# mqtt_broker = 'escape-run.sj.ifsc.edu.br'
+mqtt_broker = "test.mosquitto.org"
+mqtt_topic_subscribe = "escape-run/room/12/0"
+mqtt_topic_publish = "escape-run/player/msg"
 
 
 # ==== DFPLAYER MINI ====
 uart = UART(2, baudrate=9600, tx=17, rx=16)  # ajuste conforme sua ligação
+
 
 def enviar_comando_DFPlayer(cmd, param1=0, param2=0):
     """Envia comando padrão para o DFPlayer"""
@@ -41,10 +42,11 @@ def enviar_comando_DFPlayer(cmd, param1=0, param2=0):
     buf[9] = 0xEF
     uart.write(buf)
 
+
 def tocar_som():
     print("🎵 Tocando música ambiente...")
     enviar_comando_DFPlayer(0x03, 0x00, 0x01)  # toca 0001.mp3 no SD
-    
+
 
 # ==== FUNÇÕES ====
 def setup():
@@ -55,8 +57,6 @@ def setup():
     print("Sistema pronto!")
 
 
-
-
 def blink():
     for _ in range(3):
         sleep(0.1)
@@ -64,38 +64,47 @@ def blink():
         sleep(0.1)
         led.on()
 
+
 # ==== FUNÇÃO PARA LIBERAR O PRÊMIO ====
-def dispenser(1):
-     print("Prêmio sendo liberado")
-     
-    #print("🎁 Enviando comando para o dispenser liberar o prêmio...")
-    #client.publish(mqtt_topic_dispenser, b'release')
-    
+def dispencer():
+    dispencer(1)
+    print("Prêmio sendo liberado")
+
+
+# print("🎁 Enviando comando para o dispenser liberar o prêmio...")
+# client.publish(mqtt_topic_dispenser, b'release')
+
 
 def panic():
     print("PANIC! Liberando tudo!")
     door.value(1)
     gerador.off()
 
+
 def tocar_som():
     pass
+
 
 def game_start():
     # Iniciar o jogo
     pass
 
+
 def gerador_on():
     gerador.value(1)
 
+
 def gerador_off():
     gerador.value(0)
-    
+
+
 def door():
     door.off()
     led.off()
     sleep(5)
     door.on()
     led.on()
+
 
 # ==== CONEXÕES ====
 def connect_wifi():
@@ -104,59 +113,69 @@ def connect_wifi():
     wlan.connect(wifi_ssid, wifi_password)
 
     while not wlan.isconnected():
-        print('Connecting to Wi-Fi...')
+        print("Connecting to Wi-Fi...")
         sleep(1)
-        
-    print('Connected to Wi-Fi!')
+
+    print("Connected to Wi-Fi!")
 
 
 def connect_mqtt():
     client = MQTTClient(mqtt_client_id, mqtt_broker)
     client.connect()
     client.set_callback(callback)
-    print('Connected to MQTT broker!')
+    print("Connected to MQTT broker!")
     led.on()
     return client
 
-##mensagens de confirmação 
+
+##mensagens de confirmação
 def callback(topic, payload):
     msg = payload.decode()
-    print('Received message:', msg)
-    
-    if msg == 'blink':
+    print("Received message:", msg)
+
+    if msg == "blink":
         blink()
-    elif msg == 'panic':
+    elif msg == "panic":
         panic()
-    elif msg == 'door_lock':
-        door(0)
-    elif msg == 'door_open'
-        door(1)
-    elif msg == 'gerador_on':
+    elif msg == "door_lock":
+        door(0) == "lock"
+        led.off()
+        print("Porta fechada")
+        sleep(0, 1)
+        door(1) == "open"
+        led.on()
+        print("Porta aberta")
+        blink(0, 5)
+    # elif msg == 'door_on'
+    # door(1)
+    # door(on)
+    elif msg == "gerador_on":
         gerador.on()
-    elif msg == 'gerador_off':
+    elif msg == "gerador_off":
         gerador.off()
-    elif msg == 'start':
+    elif msg == "start":
         game_start()
-   
-    elif msg == 'reset':
+
+    elif msg == "reset":
         reset()
-    elif msg =='Liberando prêmio'
+    elif msg == "Liberando prêmio":
         dispencer(1)
         sleep(5)
-        dispencer(0) 
+        dispencer(0)
+
 
 def subscribe(client):
     client.subscribe(mqtt_topic_subscribe)
-    print('Subscribed to device topic:', mqtt_topic_subscribe)
-    
-    
-if _name_ == '_main_':
+    print("Subscribed to device topic:", mqtt_topic_subscribe)
+
+
+if _name_ == "_main_":
     setup()
     connect_wifi()
     mqtt_client = connect_mqtt()
     subscribe(mqtt_client)
-        
+
     while True:
-        mqtt_client.publish(mqtt_topic_publish, 'ping')
+        mqtt_client.publish(mqtt_topic_publish, "ping")
         mqtt_client.check_msg()
         sleep(1)
