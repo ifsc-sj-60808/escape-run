98d5da81551c4ca4c26d474226b7dc658023c298
Ederson Torresini <etorresini@ifsc.edu.br>
2025-10-31 15:20:57 -0300

    Iniciar cenas finais Fixes #201

--- src/scenes/scene13.ts
+++ src/scenes/scene13.ts
@@ -4,4 +4,16 @@ export class Scene13 extends Scene {
   constructor() {
     super({ key: "Scene13" })
   }
+
+  create() {
+    this.add.image(225, 400, "scene13-background")
+
+    this.add
+      .text(225, 640, "Final\nBônus!", {
+        fontFamily: "Sixtyfour",
+        fontSize: "48px",
+        color: "#333333"
+      })
+      .setOrigin(0.5)
+  }
 }
