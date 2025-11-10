972fea43e1708a6a5b69e15d7d21159e7ca5b960
LucasTesta7 <lucas.d03@aluno.ifsc.edu.br>
2025-11-10 20:21:53 +0000

    cena 7 e 8 ESP

--- devices/esp/scene7_8/0/main.py
+++ devices/esp/scene7_8/0/main.py
@@ -4,8 +4,7 @@ from umqtt.robust import MQTTClient  # pyright: ignore[reportMissingImports]
 from time import sleep
 
 led = Pin(2, Pin.OUT)
-vault = Pin(3, Pin.OUT)
-sensor_presenca = Pin(4, Pin.IN)
+vault = Pin(23, Pin.OUT)
 
 wifi_ssid = "escape-run"
 wifi_password = "escape-run"
@@ -15,15 +14,13 @@ mqtt_broker = "escape-run.sj.ifsc.edu.br"
 mqtt_topic_subscribe = "escape-run/devices/scene7/0"
 mqtt_topic_publish = "escape-run/player/scene"
 
-vault_password = "859"
-
 
 def setup():
     led.off()
-    vault.on()
+    vault.off()
 
     blink()
-    sleep(30)
+    #sleep(30)
     blink()
 
 
@@ -35,16 +32,9 @@ def blink():
         led.on()
 
 
-def open_vault():
-    global vault
-    vault.off()
-
-    mqtt_client.publish(mqtt_topic_publish, "Scene8")
-
-
-def close_vault():
-    global vault
-    vault.on()
+def panic():
+    # Liberar todas as portas e travas
+    pass
 
 
 def connect_wifi():
@@ -53,57 +43,58 @@ def connect_wifi():
     wlan.connect(wifi_ssid, wifi_password)
 
     while not wlan.isconnected():
-        print("Conectando ao Wi-Fi...")
+        print("Connecting to Wi-Fi...")
         sleep(1)
 
-    print("Conectado ao Wi-Fi!")
+    print("Connected to Wi-Fi!")
 
 
 def connect_mqtt():
     client = MQTTClient(mqtt_client_id, mqtt_broker)
-    client.set_callback(callback)
     client.connect()
-    print("Conectado ao broker MQTT!")
-
+    client.set_callback(callback)
+    print("Connected to MQTT broker!")
     led.on()
-
     return client
 
 
+def open_vault():
+    vault.on()
+    print("Cofre aberto!")
+    
+    mqtt_client.publish("escape-run/player/scene", "Scene8")
+    print("Mudando para cena 8...")
+
+
 def callback(topic, payload):
-    msg = payload.decode().strip()
-    print("Mensagem recebida:", msg)
+    msg = payload.decode()
+    print("Received message:", msg)
 
-    if msg == "859":
+    if msg == "blink":
+        blink()
+        
+    elif msg == "panic":
+        panic()
+        
+    elif msg == "859":
         open_vault()
-
-    elif msg == "close":
-        close_vault()
-
+        
     elif msg == "reset":
-        print("Reiniciando ESP32...")
         reset()
 
 
 def subscribe(client):
     client.subscribe(mqtt_topic_subscribe)
-    print("Assinado em:", mqtt_topic_subscribe)
+    print("Subscribed to device topic:", mqtt_topic_subscribe)
 
 
 if __name__ == "__main__":
     setup()
     connect_wifi()
-
     mqtt_client = connect_mqtt()
     subscribe(mqtt_client)
 
-    presenca_detectada = False
-
     while True:
-        if not presenca_detectada and sensor_presenca.value() == 1:
-            print("Presença detectada! Indo para Scene7...")
-            mqtt_client.publish(mqtt_topic_publish, "Scene7")
-            presenca_detectada = True
-
+        mqtt_client.publish(mqtt_topic_publish, "ping")
         mqtt_client.check_msg()
         sleep(1)
