a975d310aaefcab3998a7b9ebb8107e338ea3297
wasto4 <149070352+wasto4@users.noreply.github.com>
2025-11-24 18:51:05 +0000

    adicionado video e alterado senha

--- devices/rasp/scene2/play_video.py
+++ devices/rasp/scene2/play_video.py
@@ -10,7 +10,7 @@ MQTT_PORT = 1883
 MQTT_SUBSCRIBE_TOPIC = "escape-run/devices/scene2-0"
 MQTT_PUBLISH_TOPIC = "escape-run/player/scene"
 MQTT_PUBLISH_PAYLOAD = "Scene3"
-PASSWORD = "केऔचूं"
+PASSWORD = "093"
 
 vlc_process = None
 
