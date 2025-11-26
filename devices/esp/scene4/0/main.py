d5981611f81ecaea76883b0e89d2c5029dacf7d5
s-cittadin <sofia.c11@aluno.ifsc.edu.br>
2025-11-26 17:47:17 +0000

    atualizar código esp32 da cena 4 Fixes #265

--- devices/esp/scene4/0/main.py
+++ devices/esp/scene4/0/main.py
@@ -1,15 +1,33 @@
-from machine import Pin, reset  # pyright: ignore[reportMissingImports]
+from machine import Pin, reset, PWM, SoftI2C  # pyright: ignore[reportMissingImports]
 import network  # pyright: ignore[reportMissingImports]
 from umqtt.simple import MQTTClient  # pyright: ignore[reportMissingImports]
 from time import sleep
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
+
+# filtro lcd:
+
+# Configurações I2C (pinos padrão para ESP32)
+I2C_ADDR = 0x27  # Endereço I2C comum para o módulo PCF8574
+totalRows = 2
+totalColumns = 16
+
+# Inicializa I2C (GPIO 22 para SCL, GPIO 21 para SDA)
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)
+lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
+
+# Configurações do PWM para o Backlight
+# Escolha um pino GPIO para controlar o backlight (ex: GPIO 13)
+BACKLIGHT_PIN = 13
+pwm = PWM(Pin(BACKLIGHT_PIN), freq=1000)  # Inicializa PWM com 1KHz
+
 
 device = "scene4"
 device_number = "0"
 led = Pin(2, Pin.OUT)
 luz_gerador_ligado = Pin(26, Pin.OUT)
 luz_gerador_desligado = Pin(25, Pin.OUT)
-filtro_lcd = Pin(32, Pin.OUT)
-sensor_metal = Pin(33, Pin.IN)
+sensor_metal = Pin(33, Pin.IN, Pin.PULL_DOWN)
 wifi_ssid = "escape-run"
 wifi_password = "escape-run"
 broker = "escape-run.sj.ifsc.edu.br"
@@ -27,14 +45,18 @@ def setup():
     print("Iniciando código...")
     led.off()
     luz_gerador_ligado.off()
-    luz_gerador_desligado.off()
-    filtro_lcd.off()
+    luz_gerador_desligado.on()
+
+    lcd.clear()
+    lcd.backlight_off()
+    lcd.putstr("Energia         Desligada")
+
     print("Sensores e atuadores configurados.")
 
 
 def sensor_pre():
     print("30s para evacuar local...")
-    sleep(30)
+    # sleep(30)
     print("Tempo esgotado!")
 
 
@@ -86,9 +108,16 @@ def read_sensor_metal():
     global last_read, scanning
     current_read = sensor_metal.value()
     if current_read > last_read:
-        print("Novo jogador detectado!")
+        print("gerador ligado!")
         mqtt_client.publish(topic_publish, "Scene5")
         blink()
+        luz_gerador_ligado.on()
+        luz_gerador_desligado.off()
+
+        lcd.clear()
+        lcd.backlight_on()
+        lcd.putstr("Energia         Ligada!")
+
         scanning = False
     last_read = current_read
 
